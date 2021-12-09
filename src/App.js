543b7645cb27b282b2316c06b54856505b001874
AltairBarahona <altair.profesional@gmail.com>
2021-12-08 19:58:12 -0500

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Activation from "./components/Activation";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
+      <Routes>
+        <Route path="/activate/:token" element={<Activation />} />
+        <Route path="/" element={<Navigate to ="/activate/:token" />}/>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/components/Activation.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+const Activation = () => {
+
+    const [notification, setNotification] = useState(null);
+    const [statusError, setStatusError] = useState(null);
+    const { token } = useParams();
+
+    const onHandlerClick = () => {
+
+        console.log("Activation button clicked");
+        console.log(token);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "token": token
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        //change the url to create http request
+        // check the status of the response
+        // check the method of http request
+        fetch("http://localhost:3000/api/login/email-activate", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(result)
+                setNotification("Activation successful");
+                setStatusError(false);
+            })
+            .catch(error => {
+                console.log('error', error)
+                setNotification("Activation failed, check the console for more details");
+                setStatusError(true);
+            });
+    }
+
+    /*
+    const onHandlerClickError = () => {
+        setNotification("This is a error notification");
+        setStatusError(true);
+    }
+
+    const onHandlerClickSuccess = () => {
+        setNotification("This is a success notification");
+        setStatusError(false);
+    }
+    */
+
+    return (
+        <div className="container mt-5">
+
+            {notification && (
+                <div className={
+                    `alert ${statusError ?
+                        'alert alert-danger' :
+                        'alert-primary'}`
+                }
+                    role="alert">
+                    {notification}
+                </div>
+            )}
+
+            <button className="btn btn-primary" onClick={onHandlerClick}>Activation</button>
+            {/*
+            <button className="btn btn-danger" onClick={onHandlerClickError}>Error</button>
+            <button className="btn btn-success" onClick={onHandlerClickSuccess}>Success</button>
+            */}
+        </div>
+    )
+
+}
+
+export default Activation;
