e298e52062ee18bfcc4dc622edd468e662985375
AltairBarahona <altair.profesional@gmail.com>
2022-02-16 18:46:35 -0500

    Añadida ruta y endpoint para recuperar contraseña

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Activation from "./components/Activation";
+import Reset from "./components/Reset";
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
 
       <Routes>
         <Route path="/activate/:token" element={<Activation />} />
+        <Route path="/resetpassword/:token" element={<Reset />} />
         <Route path="/" element={<Navigate to ="/activate/:token" />}/>
       </Routes>
 
---
+++ src/components/Reset.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+const Reset = () => {
+
+    const [notification, setNotification] = useState(null);
+    const [statusError, setStatusError] = useState(null);
+    const [newPassword, setNewPassword] = useState('');
+    const { token } = useParams();
+
+    const onHandlerClick = () => {
+
+        console.log("Reset button clicked");
+        console.log(token);
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "resetLink": token,
+            "newPass": newPassword
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        //change the url to create http request
+        // check the status of the response
+        // check the method of http request
+        // fetch(`http://${process.env.IP_ELASTICA_EC2_AWS}:3000/api/login/email-activate`, requestOptions)
+        //fetch("http://3.23.34.169:3000/api/login/reset-password", requestOptions)
+        //fetch("http://localhost:3000/api/login/reset-password", requestOptions)
+        fetch("http://3.23.34.169:3000/api/login/reset-password", requestOptions)
+
+
+            .then(response => {response.text()
+            console.log(response)
+            })
+            .then(result => {
+                console.log(result)
+                setNotification("!Contraseña actualizada con éxito!");
+                setStatusError(false);
+            })
+            .catch(error => {
+                console.log('Error', error)
+                setNotification("Error en la actualización");
+                setStatusError(true);
+            });
+    }
+
+    /*
+    const onHandlerClickError = () => {
+        setNotification("This is a error notification");
+        setStatusError(true);
+    }
+
+    const onHandlerClickSuccess = () => {
+        setNotification("This is a success notification");
+        setStatusError(false);
+    }
+    */
+    const onHandlerResetPassword= (event)=>{
+        console.log(event.target.value)
+        setNewPassword(event.target.value)
+    };
+    return (
+        <div className="container mt-5">
+
+            {notification && (
+                <div className={
+                    `alert ${statusError ?
+                        'alert alert-danger' :
+                        'alert-primary'}`
+                }
+                    role="alert">
+                    {notification}
+                </div>
+            )}
+
+            
+            {/*
+            <button className="btn btn-danger" onClick={onHandlerClickError}>Error</button>
+            <button className="btn btn-success" onClick={onHandlerClickSuccess}>Success</button>
+            */}
+        
+        <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Ingresa tu nueva contraseña</label>
+            <input type="password" className="form-control" value={newPassword} onChange={onHandlerResetPassword}/>
+            <div id="" className="form-text">Tu información se cifrada y se guarda de forma segura.</div>
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={onHandlerClick}>Actualizar contraseña</button>
+        
+        </div>
+    )
+
+}
+
+export default Reset;
